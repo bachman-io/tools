bdaaead26702eefa0af819e3cd24b9281b285de1
Collin Bachman <collin.bachman@gmail.com>
2019-02-23 16:34:41 -0500

    Run AniList update every hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('wanikani:update')->everyTenMinutes();
+        $schedule->command('anilist:update')->hourly();
     }
 
     /**
